c2e004a1b4c15e5948123a7646d4cb5fbc799f08
Owen Lewis <owenalewis@gmail.com>
2023-01-05 16:25:15 +0000

    Creates operate function in script.js Operate function takes 3 arguments. An operator and 2 numbers. It then tests the operator before calling/returning the appropriate calculation function.

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
     return a / b;
 };
+
+const operate = function(operator, a, b) {
+    operator = operator.toLowerCase();
+    a = Number(a);
+    b = Number(b);
+
+    // console.log(operator, a, b);
+
+    if ( operator === '+' ) {
+        // console.log('add');
+        return add(a, b);
+    } else if ( operator === '-' ) {
+        // console.log('subtract');
+        return subtract(a, b);
+    } else if ( operator === '*' || operator.toLowerCase() === 'x' ) {
+        // console.log('multiply');
+        return multiply(a, b);
+    } else if ( operator === '/' ) {
+        // console.log('divide');
+        return divide(a, b);
+    }
+};
