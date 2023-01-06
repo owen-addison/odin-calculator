b0f2610a099ce4eba4e0cb15cf85fe2a152bd2e3
Owen Lewis <owenalewis@gmail.com>
2023-01-06 16:39:48 +0000

    Refractors code for button functionality Comments out old code and moves functionality for buttons to separate functions for better readability.
    Updates class and id names in html.
    
    Updates css file to provide better guide when developing.

--- script.js
+++ script.js
@@ -5,87 +5,124 @@ let op = '';
 let objArray = [];
 let multIndex, divIndex, subIndex, plusIndex;
 
-const display = document.querySelector('#display');
+let prevNum = '';
+let currNum = '';
+let operator = '';
 
-const buttons = document.querySelectorAll('button');
+const currentDisplay = document.querySelector('.currentNumber');
+const previousDisplay = document.querySelector('.previousNumber');
+
+const buttons = document.querySelectorAll('.btn-num');
+
+const operators = document.querySelectorAll('.btn-op');
 
 const clrBtnID = document.getElementById('btn-clr').id;
 
 const eqlBtnID = document.getElementById('btn-eql').id;
 
-// Changes display on click of button
-buttons.forEach((btn, i) => {
-    const btnText = btn.textContent;    // Get button text
-    const btnID = btn.id;   // Get button ID
-    const btnClass = btn.className;
-
-    // Add operator button to objArray as objects
-    if (btnClass === 'btn-op') {
-        objArray.push(btnObj = {
-            name: btnID,
-            text: btnText,
-            clicked: false,
-        });
-    }
-    
-    // Add event listeners for button
-    btn.addEventListener('click', () => {
-        
-        // Set display value depending on button input
-        if (btnID === clrBtnID) {
-            
-            dispVal = '';   // If clear button empty string
-            a = '';
-            b = '';
-            changeDisplay(dispVal); // Display new string
-            
-        } else if (btnClass === 'btn-num') {
-            
-            // If any of the clicked booleans is true in objArray, call operate function and display result
-            
-            // Filter objArray for clicked objects
-            const clicked = objArray.filter(function(obj) {
-                if (obj.clicked == true) {
-                    return true;
-                }
-            });
-            
-            const obj = clicked[0];
-            
-            dispVal = dispVal.concat(btnText);  // If number button append it to end of string
-            changeDisplay(dispVal); // Display new string
-            
-        } else if (btnClass === 'btn-op') {
-            const index = objArray.findIndex(obj => obj.name === btnID);
-
-            const opObj = objArray[index];
-            opObj.clicked = true;
-            // console.log(opObj);
-
-            // const filtered = objArray.filter(function(obj) {
-            //     if (obj.name === btnID) {
-            //         return true;
-            //     }
-            // });
-            
-            
-            // console.table(filtered);
-            console.table(opObj);
-            
-            a = dispVal;    // Store current display value
-            op = btnText;   // Store operator button text
-            dispVal = '';   // Reset
-            
-        } else if (btnID === eqlBtnID) {
-            
-            b = dispVal;
-            dispVal = operate(op, a, b);
-            // console.log(a, b, dispVal);
-            changeDisplay(dispVal); // Display new string
-        } 
-        
+// Add event listener to number buttons
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handleNumber(e.target.textContent);
     });
 });
+
+function handleNumber(number) {
+    currNum += number;  // If number button append it to end of string
+    changeCurrentDisplay(currNum); // Display new string
+}
+
+// Add event listener to operate buttons
+operators.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        handleOperator(e.target.textContent);
+    });
+});
+
+function handleOperator(op) {
+    operator = op;
+    prevNum = currNum;
+    changePreviousDisplay(prevNum + operator);
+    currNum = '';
+    changeCurrentDisplay(currNum);
+}
+
+// OLD FUNCTION
+// buttons.forEach((btn, i) => {
+//     const btnText = btn.textContent;    // Get button text
+//     const btnID = btn.id;   // Get button ID
+//     const btnClass = btn.className;
+
+//     // Add operator button to objArray as objects
+//     if (btnClass === 'btn-op') {
+//         objArray.push(btnObj = {
+//             name: btnID,
+//             text: btnText,
+//             clicked: false,
+//         });
+//     }
+    
+//     // Add event listeners for button
+//     btn.addEventListener('click', () => {
+        
+//         // Set display value depending on button input
+//         if (btnID === clrBtnID) {
+            
+//             dispVal = '';   // If clear button empty string
+//             a = '';
+//             b = '';
+//             changeDisplay(dispVal); // Display new string
+            
+//         } else if (btnClass === 'btn-num') {
+//             let opObj = {};
+//             let clicked = [];
+//             // If any of the clicked booleans is true in objArray, call operate function and display result
+            
+//             // Filter objArray for clicked objects
+//             clicked = objArray.filter(function(obj) {
+//                 if (obj.clicked == true) {
+//                     return true;
+//                 }
+//             });
+            
+//             opObj = clicked[0];
+
+
+            
+//             dispVal = dispVal.concat(btnText);  // If number button append it to end of string
+//             changeDisplay(dispVal); // Display new string
+            
+//         } else if (btnClass === 'btn-op') {
+//             const index = objArray.findIndex(obj => obj.name === btnID);
+
+//             const opObj = objArray[index];
+//             opObj.clicked = true;
+//             // console.log(opObj);
+
+//             // const filtered = objArray.filter(function(obj) {
+//             //     if (obj.name === btnID) {
+//             //         return true;
+//             //     }
+//             // });
+            
+            
+//             // console.table(filtered);
+//             console.table(opObj);
+            
+//             a = dispVal;    // Store current display value
+//             op = btnText;   // Store operator button text
+//             dispVal = '';   // Reset
+            
+//         } else if (btnID === eqlBtnID) {
+            
+//             b = dispVal;
+//             dispVal = operate(op, a, b);
+//             // console.log(a, b, dispVal);
+//             changeDisplay(dispVal); // Display new string
+//         } 
+        
+//     });
+// });
 
 // Store index for each operator object in objArray
 multIndex = objArray.findIndex(obj => obj.name === 'btn-mult');
@@ -94,9 +131,14 @@ subIndex = objArray.findIndex(obj => obj.name === 'btn-sub');
 plusIndex = objArray.findIndex(obj => obj.name === 'btn-plus');
 
 // Change display function
-function changeDisplay(string) {
-    display.textContent = string;
-    console.table(objArray);
+function changeCurrentDisplay(string) {
+    currentDisplay.textContent = string;
+    // console.table(objArray);
+};
+
+function changePreviousDisplay(string) {
+    previousDisplay.textContent = string;
+    // console.table(objArray);
 };
 
 // Addition function
